7a21ba40767e877a6128960085dff1e1e18f8d56
SamaraCLima <103124423+SamaraCLima@users.noreply.github.com>
2022-06-30 16:42:43 -0300

    s5-18

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 let competidores = ['Rafael', 'Manuel', 'Daniel']
-function positions() {
-    let ordem = competidores.indexOf('Daniel')
+function positions(para) {
+    let ordem = para.indexOf('Daniel')
       if (ordem != 0) {
-        competidores.splice(ordem, 1)
-        competidores.splice(ordem-1, 0, 'Daniel')
+        para.splice(ordem, 1)
+        para.splice(ordem-1, 0, 'Daniel')
     }
       else {
         return 'Daniel é o vencedor'
     }
-  return 'Este é o pódio: ' + competidores
+  return 'Este é o pódio: ' + para
 }
 console.log(positions(competidores));
